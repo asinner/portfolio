8c18a40f1a7704e6d631d7a321519ce3c5b97407
Andrew Sinner <andrewsinner@gmail.com>
2014-09-17 22:56:23 -0700

    Allow capybara to test specs better

--- test/features/site_owner_creates_article_test.rb
+++ test/features/site_owner_creates_article_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 feature 'Site owner creates an article' do
   scenario 'they see article created' do
     visit new_article_path
-    fill_in 'Title', with: 'My Second Post'
-    fill_in 'Body', with: 'Lorem ipsum dolor...'
+    fill_in 'Title', with: articles(:new).title
+    fill_in 'Body', with: articles(:new).body
     click_on 'Create Article'
     page.text.must_include 'Article was successfully created.'
   end
--- test/features/site_owner_destroys_article_test.rb
+++ test/features/site_owner_destroys_article_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 feature 'Site owner destroys an article' do
   scenario 'they see article removed message' do
     visit articles_path
-    click_on 'Destroy'
-    page.wont_have_content('Hello, World')
+    # Instead of finding relative destroy link
+    #  use a specific article id. Allows for smarter exception checking.
+    first("##{articles(:one).id}").click_link('Delete')
+    page.wont_have_content(articles(:one).title)
   end
 end
