5d5f5b4f4030967ffd2c2742be62ef23e238b92c
Andrew Sinner <andrewsinner@gmail.com>
2014-09-17 21:13:23 -0700

    Test article creation

--- test/features/has_welcome_page_test.rb
+++ test/features/has_welcome_page_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 describe 'My welcome page', :capybara do
-  it "exists" do
+  it 'exists' do
     visit root_path
     assert(page.status_code, 200)
-    page.text.must_include "Welcome to my portfolio"
+    page.text.must_include 'Welcome to my portfolio'
   end
 end
---
+++ test/features/site_owner_creates_article_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+feature 'Site owner creates an article' do
+  scenario 'they see article created' do
+    visit new_article_path
+    fill_in 'Title', with: 'My Second Post'
+    fill_in 'Body', with: 'Lorem ipsum dolor...'
+    click_on 'Create Article'
+    page.text.must_include 'Article created'
+  end
+end
