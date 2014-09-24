1eed391bfec50d2c1fe777a83b6b33ef3f919674
Andrew Sinner <andrewsinner@gmail.com>
2014-09-24 02:00:12 -0700

    Updated test that only fails on Travis-CI

--- test/features/articles/site_owner_destroys_article_test.rb
+++ test/features/articles/site_owner_destroys_article_test.rb
@@ -4,6 +4,9 @@ feature 'Site owner destroys an article' do
   scenario 'they see article removed message' do
     sign_in
     visit articles_path
+    
+    print page.html
+    
     first("##{articles(:one).id}").click_link('Delete')
     page.wont_have_content(articles(:one).title)
   end
