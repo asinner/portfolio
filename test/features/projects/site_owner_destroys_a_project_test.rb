aafeaf3cf622d7fb13f4dc87ebad1922fc72b1fc
Andrew Sinner <andrewsinner@gmail.com>
2014-09-29 18:23:16 -0700

    Added edit and delete links back in project show

--- test/features/projects/site_owner_destroys_a_project_test.rb
+++ test/features/projects/site_owner_destroys_a_project_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 feature 'Site owner destroys a project' do
   scenario 'they see a deletion message' do
+    sign_in(:editor)
     visit project_path(projects(:two))
     click_on 'Delete'
     page.wont_have_content 'eCommerce'
